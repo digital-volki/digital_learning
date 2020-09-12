cead5c4e9e1da8fa65eb615d93685c81472a888b
Влад Шкердин <shkerdin.v@ya.ru>
2020-09-12 23:29:59 +0300

    add auth

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="main">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu:regular,bold&subset=Latin">
     <Nuxt />
   </div>
 </template>
 
 <style>
+
+*{
+  font-family: 'Ubuntu',serif;
+}
+
 body{
   width: 100%;
 }
